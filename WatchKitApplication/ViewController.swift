66ac369c92e1b67fca6c8b0045995a4298edaeb2
LeeSangchan <sangchan82@gmail.com>
2016-04-30 20:06:53 +0800

    Initial Commit

---
+++ WatchKitApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchKitApplication
+//
+//  Created by LeeSangchan on 2016. 4. 30..
+//  Copyright © 2016년 LeeSangchan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WatchKitApplicationTests/WatchKitApplicationTests.swift
@@ -0,0 +1,36 @@
+//
+//  WatchKitApplicationTests.swift
+//  WatchKitApplicationTests
+//
+//  Created by LeeSangchan on 2016. 4. 30..
+//  Copyright © 2016년 LeeSangchan. All rights reserved.
+//
+
+import XCTest
+@testable import WatchKitApplication
+
+class WatchKitApplicationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
